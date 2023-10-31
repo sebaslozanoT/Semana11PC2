7c88b7b898dc61e4c64cc821ea88b46e4d6b614e
The Joy Boy <rllontopcalosi@gmail.com>
2023-10-30 21:13:14 -0500

    Rodrigo Llontop Calosi

--- semana11pc2/src/semana11pc2/Estudiantes2023.java
+++ semana11pc2/src/semana11pc2/Estudiantes2023.java
@@ -2,77 +2,41 @@ package semana11pc2;
 import java.util.*;
 import javax.swing.*;
 import java.io.*;
-
 public class Estudiantes2023 {
     private static ArrayList<Estudiante> estudiantes;
 
     public Estudiantes2023() {
         estudiantes = new ArrayList<>();
-        cargarArchivo();
+        cargarArchivo(); // Carga los estudiantes desde un archivo
     }
 
     public void adicionar(Estudiante estudiante) {
-        //Introducestudiantese c?digo
-        estudiantes.add(estudiante);
+        estudiantes.add(estudiante); // Agrega un estudiante a la lista
     }
 
     public void eliminar(Estudiante estudiante) {
-        //Introduce c?digo
-        if( tamano()>0)
-        estudiantes.remove(estudiante);
-    }
-    public void registrar(Estudiante estudiante){
-       //1. buscar al estudiante al ultimo estudiante.
-       Estudiante estudiante1 = estudiantes.get(tamano()-1);
-       
-       
+        if (tamano() > 0)
+            estudiantes.remove(estudiante); // Elimina un estudiante de la lista si hay estudiantes
     }
 
-    public Estudiante obtener(int pos) {
-	//Introduce c?digo
-       
-        return null;
+    public void registrar(Estudiante estudiante) {
+        // Registra un nuevo estudiante
+        Estudiante estudiante1 = estudiantes.get(tamano() - 1);
     }
 
-    public Estudiante buscar(int codigo) {
-	//Introduce c?digo
-        /*for(int i = 0; i< estudiantes.size();i++);
-            if(codigo == estudiantes.get().getCodigo())
-                return estudiantes.get(1);
-	  return null;*/
-    }
-
-    public int tamano() {
-        //Introduce c?digo
-	  return estudiantes.size();
-    }
-
-    public void listarEstudiantes(){
-        //Introduce c?digo
-        for (Estudiante estudiante : estudiantes) {
-            System.out.println(estudiante.toString());
-        }
-    }
+    // ... otros métodos ...
 
     // M?todos para manipular el archivo de texto
     private void readFromInputStream(InputStream inputStream) throws IOException {
+        // Lee y procesa el archivo de estudiantes desde un flujo de entrada
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        String linea;
-        while ((linea = br.readLine()) != null) {
-            StringTokenizer st = new StringTokenizer(linea, ",");
-            createStudent(st);
-        }
-        br.close();
+        // ... el resto del código
     }
 
-	// M?todo que a?adir a los estudiantes del archivo .txt en el arrayList estudiantes.
-    private void createStudent(StringTokenizer st){
-        int codigo = Integer.parseInt(st.nextToken().trim());
-        String nombre = st.nextToken().trim();
-        int ciclo = Integer.parseInt(st.nextToken().trim());
-        double pension = Double.parseDouble(st.nextToken().trim());
-        Estudiante estudiante = new Estudiante(codigo, nombre, ciclo, pension);
-        adicionar(estudiante);
+    // M?todo que a?adir a los estudiantes del archivo .txt en el arrayList estudiantes.
+    private void createStudent(StringTokenizer st) {
+        // Crea un objeto Estudiante a partir de los datos del archivo
+        // ... el resto del código
     }
 
     // M?todos para manipular el archivo de texto
@@ -84,8 +48,7 @@ public class Estudiantes2023 {
             if (file.exists()) {
                 readFromInputStream(inputStream);
             } else
-                JOptionPane.showMessageDialog(null,
-                        "El archivo estudiantes.txt no existe");
+                JOptionPane.showMessageDialog(null, "El archivo estudiantes.txt no existe");
         } catch (Exception x) {
             JOptionPane.showMessageDialog(null, "Se produjo un error= " + x);
         }
